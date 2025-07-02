826d83bfa28b03e4ca7d8874091e88c5addc6d74
DESKTOP-EHFIDAR\user1 <hwihun5623@gmail.com>
2025-07-02 16:46:58 +0900

    LINK가 2번 import돼서 수정

--- final_front_workspace/library_project/src/component/common/Main.jsx
+++ final_front_workspace/library_project/src/component/common/Main.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import useUserStore from "../../store/useUserStore";
 import { Link } from "react-router-dom";
 
