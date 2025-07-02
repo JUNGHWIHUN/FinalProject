a173de8b5de8fd730fcc847938119fb58b0a44c3
DESKTOP-8KJ5FQ6\user1 <dnlshsk13@gmail.com>
2025-07-02 12:54:08 +0900

    백엔드 어드민 컨트롤러, 맵퍼 작성. 프론트 어드민 페이지 일부 수정

--- final_front_workspace/library_project/src/component/admin/AdminPage.jsx
+++ final_front_workspace/library_project/src/component/admin/AdminPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import axios from "axios";
-
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function AdminPage(){
 
