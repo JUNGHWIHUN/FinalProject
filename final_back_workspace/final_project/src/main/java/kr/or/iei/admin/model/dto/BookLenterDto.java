8ddf2d2ba2b42e5001edf518e716d5efeefe238c
DESKTOP-8KJ5FQ6\user1 <dnlshsk13@gmail.com>
2025-07-04 11:57:01 +0900

    대출관련 처리. dto 추가, mapper 수정. 프론트 일부 수정

---
+++ final_back_workspace/final_project/src/main/java/kr/or/iei/admin/model/dto/BookLenterDto.java
@@ -0,0 +1,15 @@
+package kr.or.iei.admin.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class BookLenterDto {
+	
+	//대출 처리를 할 때, 유저의 번호와 책의 번호를 가져올 Dto.
+	private String memberNo;
+    private String bookNo;
+}
