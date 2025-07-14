f9fc04b266414f16c81dd07be86dbc957c815ad3
DESKTOP-6PUREJP\user1 <seoght1401@gmail.com>
2025-07-14 17:35:06 +0900

    서용준 07/14 - 지도 api 마커 수정, 도서관 소개 페이지 css 추가

--- final_front_workspace/library_project/src/component/libInfo/LibInfoMain.jsx
+++ final_front_workspace/library_project/src/component/libInfo/LibInfoMain.jsx
@@ -1,31 +1,51 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import LibIntroduce from "./LibIntroduce";
 import LibUseInfo from "./LibUseInfo";
 import LibLocation from "./LibLocation";
 import LibRegulation from "./LibRegulation";
 import LeftMenu from "../common/LeftMenu";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import './LibInfo.css';
 
 export default function LibInfoMain(){
     
+    const location = useLocation();
+    const [activeMenu, setActiveMenu] = useState(""); // 활성 메뉴 상태 추가
+
     const [menuList, setMenuList] = useState([
             {url : '/libInfo/introduce', text : "도서관 소개"},
             {url : '/libInfo/useInfo', text : "이용안내"},
             {url : '/libInfo/location', text : "오시는 길"},
             {url : '/libInfo/regulation', text : "운영규정"},
         ]);
+    
+    useEffect(() => {
+            const currentPath = location.pathname;
+            const foundMenu = menuList.find(menu => currentPath.startsWith(menu.url));
+            if (foundMenu) {
+                setActiveMenu(foundMenu.text);
+            } else {
+                setActiveMenu(""); // 일치하는 메뉴가 없으면 초기화
+            }
+        }, [location.pathname, menuList]);
 
     return(
-    <>
-    <LeftMenu menuList = {menuList}/>
-
-       <Routes>
-        <Route path="introduce" element={<LibIntroduce />} />
-        <Route path="useInfo" element={<LibUseInfo />} />
-        <Route path="location" element={<LibLocation />} />
-        <Route path="regulation" element={<LibRegulation />} />
-       </Routes>
-
-    </>
+    
+    <div className="LibInfo-wrapper"> {/* 마이페이지 전체 컨테이너 */}
+        <div className="LibInfo-title-section">
+            <h1>도서관 소개</h1>
+        </div>
+        <div className="LibInfo-content-wrapper">
+        <LeftMenu menuList = {menuList} activeMenu={activeMenu}/>
+            <div className="LibInfo-main-content">
+                <Routes>
+                    <Route path="introduce" element={<LibIntroduce />} />
+                    <Route path="useInfo" element={<LibUseInfo />} />
+                    <Route path="location" element={<LibLocation />} />
+                    <Route path="regulation" element={<LibRegulation />} />
+                </Routes>
+            </div>
+        </div>
+    </div>
     )
 }
