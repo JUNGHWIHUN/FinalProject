41a8b42261262748352f0499acdd2dc698553523
DESKTOP-8KJ5FQ6\user1 <dnlshsk13@gmail.com>
2025-07-15 17:14:33 +0900

    병합 충돌

--- final_back_workspace/final_project/src/main/java/kr/or/iei/board/model/service/SuggestionService.java
+++ final_back_workspace/final_project/src/main/java/kr/or/iei/board/model/service/SuggestionService.java
@@ -44,6 +44,7 @@ public class SuggestionService {
         // 비밀글인 경우 본인만 보이거나 관리자만 보이도록 쿼리 로직 필요
 		ArrayList<BoardDto> boardList = dao.selectSuggestionList(pageInfo, loginMemberNo);
 		
+
 		HashMap<String, Object> boardMap = new HashMap<>();
 		boardMap.put("boardList", boardList);
 		boardMap.put("pageInfo", pageInfo);
